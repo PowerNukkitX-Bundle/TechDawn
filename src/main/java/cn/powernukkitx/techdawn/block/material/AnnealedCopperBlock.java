062c0d0a5b190d50a502c5204724300bf0e479f7
超神的冰凉 <2801621420@qq.com>
2023-02-04 17:58:42 +0800

    添加退火铜块

---
+++ src/main/java/cn/powernukkitx/techdawn/block/material/AnnealedCopperBlock.java
@@ -0,0 +1,19 @@
+package cn.powernukkitx.techdawn.block.material;
+
+import cn.nukkit.block.customblock.CustomBlock;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import org.jetbrains.annotations.NotNull;
+
+@AutoRegister(CustomBlock.class)
+public class AnnealedCopperBlock extends BaseMaterialBlock {
+    @NotNull
+    @Override
+    public String getNamespaceId() {
+        return "techdawn:annealed_copper_block";
+    }
+
+    @Override
+    public double getHardness() {
+        return 4;
+    }
+}
