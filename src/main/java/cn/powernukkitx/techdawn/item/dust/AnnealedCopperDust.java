6786564ea9973228c848222df2dbfd9dfa91ecb6
超神的冰凉 <2801621420@qq.com>
2023-02-05 23:46:29 +0800

    添加退火铜粉

---
+++ src/main/java/cn/powernukkitx/techdawn/item/dust/AnnealedCopperDust.java
@@ -0,0 +1,25 @@
+package cn.powernukkitx.techdawn.item.dust;
+
+import cn.nukkit.item.customitem.CustomItem;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.annotation.AutoRegisterData;
+import org.jetbrains.annotations.NotNull;
+
+@AutoRegister(CustomItem.class)
+@AutoRegisterData("#getTags")
+public class AnnealedCopperDust extends BaseDust {
+    public AnnealedCopperDust() {
+        super("techdawn:annealed_copper_dust");
+    }
+
+    @Override
+    public int getHardnessTier() {
+        return HARDNESS_ANNEALED_COPPER;
+    }
+
+    @NotNull
+    @Override
+    public String getTags() {
+        return "annealed_copper_dust copper_dust";
+    }
+}
