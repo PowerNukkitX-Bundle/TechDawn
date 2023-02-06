722df67151b601f844fcbc404bd127a2ccf7e08e
超神的冰凉 <2801621420@qq.com>
2023-02-06 13:36:26 +0800

    添加钢粉

---
+++ src/main/java/cn/powernukkitx/techdawn/item/dust/SteelDust.java
@@ -0,0 +1,25 @@
+package cn.powernukkitx.techdawn.item.dust;
+
+import cn.nukkit.item.customitem.CustomItem;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.annotation.AutoRegisterData;
+import org.jetbrains.annotations.NotNull;
+
+@AutoRegister(CustomItem.class)
+@AutoRegisterData("#getTags")
+public class SteelDust extends BaseDust {
+    public SteelDust() {
+        super("techdawn:steel_dust");
+    }
+
+    @Override
+    public int getHardnessTier() {
+        return HARDNESS_STEEL;
+    }
+
+    @NotNull
+    @Override
+    public String getTags() {
+        return "steel_dust steel iron_dust iron";
+    }
+}
