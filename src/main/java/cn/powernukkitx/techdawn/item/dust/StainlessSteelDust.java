79bd6bb37c4d59fa80d7a6dab848d493213b3db5
超神的冰凉 <2801621420@qq.com>
2023-02-06 13:40:29 +0800

    添加不锈钢粉

---
+++ src/main/java/cn/powernukkitx/techdawn/item/dust/StainlessSteelDust.java
@@ -0,0 +1,25 @@
+package cn.powernukkitx.techdawn.item.dust;
+
+import cn.nukkit.item.customitem.CustomItem;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.annotation.AutoRegisterData;
+import org.jetbrains.annotations.NotNull;
+
+@AutoRegister(CustomItem.class)
+@AutoRegisterData("#getTags")
+public class StainlessSteelDust extends BaseDust {
+    public StainlessSteelDust() {
+        super("techdawn:stainless_steel_dust");
+    }
+
+    @Override
+    public int getHardnessTier() {
+        return HARDNESS_STAINLESS_STEEL;
+    }
+
+    @NotNull
+    @Override
+    public String getTags() {
+        return "stainless_steel_dust stainless_steel steel_dust steel iron_dust iron";
+    }
+}
