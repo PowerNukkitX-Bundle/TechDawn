61f5d843295afb8cc28ea47ff4e76fbeb19e8246
超神的冰凉 <2801621420@qq.com>
2023-02-06 15:09:04 +0800

    添加青铜锭

---
+++ src/main/java/cn/powernukkitx/techdawn/item/ingot/BronzeIngot.java
@@ -0,0 +1,26 @@
+package cn.powernukkitx.techdawn.item.ingot;
+
+import cn.nukkit.item.customitem.CustomItem;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.annotation.AutoRegisterData;
+import org.jetbrains.annotations.NotNull;
+
+@AutoRegister(CustomItem.class)
+@AutoRegisterData("#getTags")
+public class BronzeIngot extends BaseIngot {
+    public BronzeIngot() {
+        super("techdawn:bronze_ingot", "techdawn-items-ingot-bronze_ingot");
+    }
+
+    @NotNull
+    @Override
+    public String getTags() {
+        return "bronze_ingot bronze";
+    }
+
+
+    @Override
+    public int getHardnessTier() {
+        return HARDNESS_BRONZE;
+    }
+}
