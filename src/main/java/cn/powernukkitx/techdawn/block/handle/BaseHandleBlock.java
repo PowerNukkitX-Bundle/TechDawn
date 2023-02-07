720edffd657278d8b12ba4ecfeda0f63407c3ec8
超神的冰凉 <2801621420@qq.com>
2023-02-07 17:09:44 +0800

    添加基本曲柄握把方块

--- src/main/java/cn/powernukkitx/techdawn/block/handle/BaseHandleBlock.java
+++ src/main/java/cn/powernukkitx/techdawn/block/handle/BaseHandleBlock.java
@@ -1,4 +1,142 @@
 package cn.powernukkitx.techdawn.block.handle;
 
-public class BaseHandleBlock {
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockTransparent;
+import cn.nukkit.block.customblock.CustomBlock;
+import cn.nukkit.block.customblock.CustomBlockDefinition;
+import cn.nukkit.block.customblock.data.Materials;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.item.Item;
+import cn.nukkit.math.BlockFace;
+import cn.nukkit.math.BlockVector3;
+import cn.nukkit.math.Vector3f;
+import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.entity.handle.BaseHandleEntity;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
+import java.util.Objects;
+
+@AutoRegister(CustomBlock.class)
+public class BaseHandleBlock extends BlockTransparent implements CustomBlock {
+    private static final Cache<BlockVector3, BaseHandleEntity> entityCache = Caffeine.newBuilder()
+            .maximumSize(1024)
+            .expireAfterWrite(Duration.ofMinutes(10))
+            .weakKeys()
+            .build();
+
+    @Override
+    public String getName() {
+        return CustomBlock.super.getName();
+    }
+
+    @Override
+    @NotNull
+    public String getNamespaceId() {
+        return "techdawn:base_handle";
+    }
+
+    @Override
+    public CustomBlockDefinition getDefinition() {
+        return CustomBlockDefinition.builder(this, Materials.builder()
+                        .any(Materials.RenderMethod.BLEND, "techdawn-blocks-transparent"))
+                .selectionBox(new Vector3f(-7f, 0f, -7f), new Vector3f(14f, 8f, 14f))
+                .collisionBox(new Vector3f(-7f, 0f, -7f), new Vector3f(14f, 8f, 14f)).build();
+    }
+
+    @Override
+    public int getId() {
+        return CustomBlock.super.getId();
+    }
+
+    public BaseHandleEntity createHandleEntity() {
+        return new BaseHandleEntity(this.getChunk(), Entity.getDefaultNBT(this.add(0.5, 0, 0.5)));
+    }
+
+    @Override
+    public boolean place(@NotNull Item item, @NotNull Block block, @NotNull Block target, @NotNull BlockFace face, double fx, double fy, double fz, @Nullable Player player) {
+        var sr = super.place(item, block, target, face, fx, fy, fz, player);
+        if (sr) {
+            this.createHandleEntity().spawnToAll();
+        }
+        return sr;
+    }
+
+    @NotNull
+    public BaseHandleEntity getHandleEntity() {
+        return Objects.requireNonNull(getHandleEntity(false));
+    }
+
+    @Nullable
+    public BaseHandleEntity getHandleEntity(boolean doNotCreate) {
+        return entityCache.get(this.asBlockVector3(), k -> {
+            var tmp = this.level.getChunkEntities(this.getChunkX(), this.getChunkZ()).values().stream()
+                    .filter(entity -> entity instanceof BaseHandleEntity)
+                    .filter(entity -> entity.getFloorX() == this.getFloorX() && entity.getFloorY() == this.getFloorY() && entity.getFloorZ() == this.getFloorZ())
+                    .findAny();
+            if (tmp.isPresent()) {
+                return (BaseHandleEntity) tmp.get();
+            } else if (!doNotCreate) {
+                var e = this.createHandleEntity();
+                e.spawnToAll();
+                return e;
+            }
+            return null;
+        });
+    }
+
+    @Override
+    public boolean onBreak(Item item) {
+        var tmp = getHandleEntity(true);
+        if (tmp != null) {
+            tmp.close();
+        }
+        entityCache.invalidate(this.asBlockVector3());
+        return super.onBreak(item);
+    }
+
+    @Override
+    public boolean canBeActivated() {
+        return true;
+    }
+
+    @Override
+    public boolean onActivate(@NotNull Item item, @Nullable Player player) {
+        getHandleEntity().onPlayerInteract(player);
+        return true;
+    }
+
+    @Override
+    public double getMaxX() {
+        return this.x + 0.875;
+    }
+
+    @Override
+    public double getMaxY() {
+        return this.y + 0.5;
+    }
+
+    @Override
+    public double getMaxZ() {
+        return this.z + 0.875;
+    }
+
+    @Override
+    public double getMinX() {
+        return this.x + 0.125;
+    }
+
+    @Override
+    public double getMinY() {
+        return this.y;
+    }
+
+    @Override
+    public double getMinZ() {
+        return this.z + 0.125;
+    }
 }
--- src/main/java/cn/powernukkitx/techdawn/entity/handle/BaseHandleEntity.java
+++ src/main/java/cn/powernukkitx/techdawn/entity/handle/BaseHandleEntity.java
@@ -1,11 +1,17 @@
 package cn.powernukkitx.techdawn.entity.handle;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.custom.CustomEntity;
 import cn.nukkit.entity.custom.CustomEntityDefinition;
+import cn.nukkit.level.Sound;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.network.protocol.EntityEventPacket;
+import cn.nukkit.network.protocol.PlaySoundPacket;
 import cn.powernukkitx.techdawn.annotation.AutoRegister;
+import cn.powernukkitx.techdawn.block.handle.BaseHandleBlock;
 
 @AutoRegister(CustomEntity.class)
 public class BaseHandleEntity extends Entity implements CustomEntity {
@@ -14,6 +20,8 @@ public class BaseHandleEntity extends Entity implements CustomEntity {
             .summonable(true)
             .spawnEgg(false)
             .build();
+
+    protected int rotatingTick = 0;
 
     public BaseHandleEntity(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -56,11 +64,45 @@ public class BaseHandleEntity extends Entity implements CustomEntity {
 
     @Override
     public boolean onUpdate(int currentTick) {
-        this.yaw += 5;
+        if (rotatingTick != 0) {
+            this.yaw += 5;
+            --this.rotatingTick;
+        }
         if (yaw > 360) {
             yaw -= 360;
         }
         updateMovement();
+        if (!(getTickCachedLevelBlock() instanceof BaseHandleBlock)) {
+            close();
+        }
         return super.onUpdate(currentTick);
+    }
+
+    protected String getSound() {
+        return "techdawn.wood_handle";
+    }
+
+    public void onPlayerInteract(Player player) {
+        if (rotatingTick != 0) {
+            return;
+        }
+        rotatingTick += 10;
+        if (player != null) {
+            var pk = new EntityEventPacket();
+            pk.eid = player.getId();
+            pk.event = EntityEventPacket.ARM_SWING;
+            player.dataPacket(pk);
+            player.getViewers().values().forEach(p -> p.dataPacket(pk));
+        }
+        {
+            var pk = new PlaySoundPacket();
+            pk.name = getSound();
+            pk.volume = 1;
+            pk.pitch = 1;
+            pk.x = this.getFloorX();
+            pk.y = this.getFloorY();
+            pk.z = this.getFloorZ();
+            this.level.addChunkPacket(this.getChunkX(), this.getChunkZ(), pk);
+        }
     }
 }
