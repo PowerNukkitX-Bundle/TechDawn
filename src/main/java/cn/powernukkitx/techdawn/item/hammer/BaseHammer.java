66f67a88267ba4abcdba874cf6aafae2c1749528
超神的冰凉 <2801621420@qq.com>
2023-02-08 22:01:44 +0800

    适配新版ModAPI

--- src/main/java/cn/powernukkitx/techdawn/item/hammer/BaseHammer.java
+++ src/main/java/cn/powernukkitx/techdawn/item/hammer/BaseHammer.java
@@ -1,16 +1,10 @@
 package cn.powernukkitx.techdawn.item.hammer;
 
-import cn.nukkit.block.Block;
-import cn.nukkit.block.BlockGravel;
-import cn.nukkit.block.BlockSand;
 import cn.nukkit.item.customitem.CustomItemDefinition;
 import cn.nukkit.item.customitem.ItemCustomTool;
 import cn.nukkit.item.customitem.data.ItemCreativeCategory;
 import cn.powernukkitx.techdawn.data.TechDawnHardness;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.IdentityHashMap;
-import java.util.Map;
 
 public abstract class BaseHammer extends ItemCustomTool implements TechDawnHardness {
     public static final String TAG = "minecraft:digger minecraft:is_tool minecraft:is_pickaxe is_hammer ";
@@ -22,7 +16,8 @@ public abstract class BaseHammer extends ItemCustomTool implements TechDawnHardn
     @Override
     public CustomItemDefinition getDefinition() {
         return CustomItemDefinition.toolBuilder(this, ItemCreativeCategory.EQUIPMENT)
-                .addExtraBlockTags(getMiningSpeedMap())
+                .addExtraBlock("minecraft:sand", Math.max(getTier() - 1, 1))
+                .addExtraBlock("minecraft:gravel", Math.max(getTier() - 1, 1))
                 .creativeGroup("itemGroup.name.pickaxe")
                 .allowOffHand(true)
                 .tag((TAG + getTags()).split(" +"))
@@ -50,14 +45,4 @@ public abstract class BaseHammer extends ItemCustomTool implements TechDawnHardn
 
     @Override
     public abstract int getAttackDamage();
-
-    /**
-     * @return 此工具除了镐子以外的方块的挖掘加速表
-     */
-    protected Map<Block, Integer> getMiningSpeedMap() {
-        var map = new IdentityHashMap<Block, Integer>(2);
-        map.put(new BlockSand(), Math.max(getTier() - 1, 1));
-        map.put(new BlockGravel(), Math.max(getTier() - 1, 1));
-        return map;
-    }
 }
