8fbb2fbfa166bfa82e344146b6bf98fa52c79f08
超神的冰凉 <2801621420@qq.com>
2023-03-27 16:41:01 +0800

    添加石子

---
+++ src/main/java/cn/powernukkitx/techdawn/item/nugget/BaseNugget.java
@@ -0,0 +1,29 @@
+package cn.powernukkitx.techdawn.item.nugget;
+
+import cn.nukkit.item.customitem.CustomItemDefinition;
+import cn.nukkit.item.customitem.ItemCustom;
+import cn.nukkit.item.customitem.data.ItemCreativeCategory;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.powernukkitx.techdawn.data.TechDawnHardness;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class BaseNugget extends ItemCustom implements TechDawnHardness {
+    public BaseNugget(@NotNull String id) {
+        super(id, null, "techdawn-items-nugget-" + id.substring(id.lastIndexOf(":") + 1));
+    }
+
+    @Override
+    public CustomItemDefinition getDefinition() {
+        return CustomItemDefinition.simpleBuilder(this, ItemCreativeCategory.ITEMS)
+                .tag(getTags().split(" +"))
+                .customBuild(nbt -> {
+                    var component = nbt.getCompound("components");
+                    component.putCompound("minecraft:fuel", new CompoundTag().putFloat("duration", 1));
+                });
+    }
+
+    @NotNull
+    public String getTags() {
+        return "";
+    }
+}
